1dcd3f21389dd8adc3978407d6b22b095cc8bb7d
Dumpling29 <malvinmadumpling@gmail.com>
2024-11-09 00:55:06 +0800

    Testing if main.js script is being run

--- public/main.js
+++ public/main.js
@@ -1,3 +1,6 @@
+console.log("main.js is loaded");
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
